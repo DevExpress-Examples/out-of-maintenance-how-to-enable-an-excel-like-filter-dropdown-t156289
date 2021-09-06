57d938ff1a33b6fd5c9fbe7cbe88d7dadda1bd8f
DevExpressExampleBot <fakeEmail>
2021-09-06 20:55:52 +0300

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -4,22 +4,26 @@ Imports System.Linq
 Imports System.Windows
 
 Namespace DXGridSample
-    Partial Public Class MainWindow
-        Inherits Window
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Property Items() As ObservableCollection(Of Item)
-        Public Sub New()
-            Items = New ObservableCollection(Of Item)()
-            For i As Integer = 0 To 99
-                Items.Add(New Item With {.Id = i, .Name = "Name" & i, .IsEnabled = i Mod 2 = 0})
-            Next i
-            DataContext = Me
-            InitializeComponent()
-        End Sub
-    End Class
-    Public Class Item
-        Public Property Id() As Integer
-        Public Property Name() As String
-        Public Property IsEnabled() As Boolean
-    End Class
+		Public Property Items() As ObservableCollection(Of Item)
+		Public Sub New()
+			Items = New ObservableCollection(Of Item)()
+			For i As Integer = 0 To 99
+				Items.Add(New Item With {
+					.Id = i,
+					.Name = "Name" & i,
+					.IsEnabled = i Mod 2 = 0
+				})
+			Next i
+			DataContext = Me
+			InitializeComponent()
+		End Sub
+	End Class
+	Public Class Item
+		Public Property Id() As Integer
+		Public Property Name() As String
+		Public Property IsEnabled() As Boolean
+	End Class
 End Namespace
